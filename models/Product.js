94dd7f242e1da8dc370aa97410334b2c98c4a4c9
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2026-02-12 11:29:52 +0530

    Create Product.js

---
+++ models/Product.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, default: 0 },
+  image: { type: String, default: '' },
+});
+
+export default mongoose.models.Product || mongoose.model('Product', ProductSchema);
