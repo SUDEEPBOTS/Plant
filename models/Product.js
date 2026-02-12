3ccac172dc3f61b9ea0711db3e67a98cd7dc02c7
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2026-02-12 14:38:54 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: { type: Number, required: true }, // Petti Rate
+  pricePerBottle: { type: Number, default: 0 }, // New: Ek Bottle Ka Rate
   stock: { type: Number, default: 0 },
   image: { type: String, default: '' },
 });
