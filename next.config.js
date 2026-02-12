41e6ef6c90e51d65eec739792cc0c1799d5bc072
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2026-02-12 11:28:46 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    // Public variables agar chahiye ho, par hum secure API use karenge
+  }
+}
+
+module.exports = nextConfig
